f59e8be1ed4483f6de695deb8ddeb061514c27ef
JohnnyDbol <jonathansdunlap@gmail.com>
2023-07-26 03:50:42 -0500

    2 files modified

--- components/FallingWords.js
+++ components/FallingWords.js
@@ -1,6 +1,73 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const wordsList = ['apple', 'banana', 'orange', 'grapes', 'kiwi'];
+//const wordsList = ['apple', 'banana', 'orange', 'grapes', 'kiwi'];
+const wordsList = [
+  // Emo Bands
+  'Jimmy Eat World',
+  'Dashboard Confessional',
+  'Brand New',
+  'Sunny Day Real Estate',
+  'The Get Up Kids',
+  'Taking Back Sunday',
+  'Thursday',
+  'The Promise Ring',
+  'Mineral',
+  'American Football',
+  'The Juliana Theory',
+  'Braid',
+  'Moss Icon',
+  'Texas Is the Reason',
+  'Saves the Day',
+  'Cap n Jazz',
+  'Rites of Spring',
+  'Sunny Day Real Estate',
+  'The World Is a Beautiful Place & I Am No Longer Afraid to Die',
+  'Rainer Maria',
+
+  // Punk Bands
+  'Green Day',
+  'The Offspring',
+  'Rancid',
+  'Bad Religion',
+  'NOFX',
+  'The Vandals',
+  'Pennywise',
+  'Social Distortion',
+  'Anti-Flag',
+  'Lagwagon',
+  'The Bouncing Souls',
+  'Descendents',
+  'NOFX',
+  'Dropkick Murphys',
+  'The Casualties',
+  'The Distillers',
+  'Bad Brains',
+  'Black Flag',
+  'The Clash',
+  'Ramones',
+
+  // Ska Bands
+  'The Mighty Mighty Bosstones',
+  'Reel Big Fish',
+  'Less Than Jake',
+  'Streetlight Manifesto',
+  'Operation Ivy',
+  'Sublime',
+  'Mad Caddies',
+  'Catch 22',
+  'The Aquabats',
+  'Goldfinger',
+  'Skankin Pickle',
+  'The Specials',
+  'Toots and the Maytals',
+  'The Selecter',
+  'Mustard Plug',
+  'The Pietasters',
+  'Big D and the Kids Table',
+  'Fishbone',
+  'Save Ferris',
+  'The Slackers',
+];
 
 const FallingWords = () => {
   const [words, setWords] = useState([]);
@@ -12,7 +79,7 @@ const FallingWords = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       const randomWord =
-        wordsList[Math.floor(Math.random() * wordsList.length)];
+        wordsList[Math.floor(Math.random() * wordsList.length)].toLowerCase();
       setWords([randomWord]);
       setUserInput('');
       setTotalWords((total) => total + 1); // Increment total words counter
@@ -63,16 +130,18 @@ const FallingWords = () => {
 
   return (
     <>
-      <div className="navbar bg-base-100">
-        <a className="navbar-start">
+      <div className="flex ... ">
+        <a className="w-1/4">
           <h2>Total Words: {totalWords}</h2>
         </a>
-        <a className="navbar-end">
+        <a className="w-1/4">
           <h1>Correct Words: {correctWords}</h1>
         </a>
       </div>
-      <div className="box">
-        <span className="falling-word">{renderWord()}</span>
+      <div className="box prose lg:prose-l">
+        <span className="falling-word">
+          <h2>{renderWord()}</h2>
+        </span>
         <input
           ref={inputRef}
           type="text"
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import FallingWords from '../components/FallingWords.js';
 export default function Home() {
   return (
     <>
-      <div className="chat chat-start">
+      <div className="chat chat-start prose lg:prose-l">
         <div className="chat-bubble chat-bubble-primary">
           <h1>Falling Words</h1>
         </div>
